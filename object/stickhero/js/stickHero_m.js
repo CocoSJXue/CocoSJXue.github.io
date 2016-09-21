1b00e80ea4cef8887b24f78ffd52ff45efdc65d2
CocoSJXue <xuesujuan2014@163.com>
2016-09-21 12:17:00 +0800

    _m

--- object/stickhero/js/stickHero_m.js
+++ object/stickhero/js/stickHero_m.js
@@ -1,9 +1,8 @@
-var num=1/window.devicePixelRatio;
-document.write('<meta name="viewport" content="width=device-width,initial-scale='+num+',minimum-scale='+num+',maximum-scale='+num+',user-scalable=no"/>')
-var fz=document.documentElement.clientWidth / 10;
-document.getElementsByTagName("html")[0].style.fontSize = fz+"px";
-
 $(function(){
+	var num=1/window.devicePixelRatio;
+	$(document).append('<meta name="viewport" content="width=device-width,initial-scale='+num+',minimum-scale='+num+',maximum-scale='+num+',user-scalable=no"/>')
+	var fz=document.documentElement.clientWidth / 10;
+	document.getElementsByTagName("html")[0].style.fontSize = fz+"px";
 	var $section = $('section');
 	var $scoreBox = $('#scoreBox');
 	var $start = $('#start');
